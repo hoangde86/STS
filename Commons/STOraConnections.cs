d4bbd8b8d962a3d7ad928b1b7bb8ac50e719c496
Hoang <hoangde_86@yahoo.com>
2021-12-08 01:45:24 +0700

    t

---
+++ Commons/STOraConnections.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Commons
+{
+    /// <summary>
+    /// Class Connection
+    /// </summary>
+    public class STConnections
+    {
+        private static string _Server;
+
+        private static string _Database;
+
+        private static string _Username;
+
+        private static string _Password;
+
+        public static string CNStr
+        {
+            get
+            {
+                return string.Concat(new string[] { "Server=", _Server, ";database=", _Database, ";uid=", _Username, ";pwd=", _Password, ";Connect Timeout=9999;" });
+            }
+        }
+
+        public static string Database
+        {
+            get
+            {
+                return IConnections._Database;
+            }
+            set
+            {
+                IConnections._Database = value;
+            }
+        }
+
+        public static string Password
+        {
+            get
+            {
+                return IConnections._Password;
+            }
+            set
+            {
+                IConnections._Password = value;
+            }
+        }
+
+        public static string Server
+        {
+            get
+            {
+                return IConnections._Server;
+            }
+            set
+            {
+                IConnections._Server = value;
+            }
+        }
+
+        public static string Username
+        {
+            get
+            {
+                return IConnections._Username;
+            }
+            set
+            {
+                IConnections._Username = value;
+            }
+        }
+
+        public IConnections()
+        {
+        }
+    }
+}
---
+++ Commons/STSqlConnections.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Commons
+{
+    /// <summary>
+    /// Class Connection
+    /// </summary>
+    public class STSqlConnections
+    {
+        private static string _Server;
+
+        private static string _Database;
+
+        private static string _Username;
+
+        private static string _Password;
+
+        public static string CNStr
+        {
+            get
+            {
+                return string.Concat(new string[] { "Server=", _Server, ";database=", _Database, ";uid=", _Username, ";pwd=", _Password, ";Connect Timeout=9999;" });
+            }
+        }
+
+        public static string Database
+        {
+            get
+            {
+                return _Database;
+            }
+            set
+            {
+                _Database = value;
+            }
+        }
+
+        public static string Password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                _Password = value;
+            }
+        }
+
+        public static string Server
+        {
+            get
+            {
+                return _Server;
+            }
+            set
+            {
+                _Server = value;
+            }
+        }
+
+        public static string Username
+        {
+            get
+            {
+                return _Username;
+            }
+            set
+            {
+                _Username = value;
+            }
+        }
+
+        public STSqlConnections()
+        {
+        }
+    }
+}
